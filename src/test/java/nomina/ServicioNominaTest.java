1409cda2c30a1c77d7abd812bd6da03f86c8b744
Natasha <nmlc0001@ce.pucmm.edu.do>
2025-05-30 20:55:30 -0400

    feat: all the test for de ServicioNominaTest

--- src/test/java/nomina/ServicioNominaTest.java
+++ src/test/java/nomina/ServicioNominaTest.java
@@ -7,6 +7,7 @@ import org.example.entities.TipoEmpleado;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ServicioNominaTest {
 
@@ -15,7 +16,7 @@ public class ServicioNominaTest {
         Empleado empleado = new Empleado("Natasha", TipoEmpleado.FULL_TIME, 400);
         ServicioNomina servicio = new ServicioNomina();
         double salario = servicio.calcularNomina(empleado, 40, false);
-        assertEquals(16500, salario); // 40h x 400
+        assertEquals(16500, salario);
     }
 
     @Test
@@ -24,6 +25,34 @@ public class ServicioNominaTest {
         ServicioNomina servicio = new ServicioNomina();
         double salario = servicio.calcularNomina(empleado, 45, false);
         assertEquals(10000, salario);
+    }
+
+    @Test
+    void salarioSinHorasExtraParaPartTime() {
+        Empleado empleado = new Empleado("Natasha", TipoEmpleado.PART_TIME, 400);
+        ServicioNomina servicio = new ServicioNomina();
+        double salario = servicio.calcularNomina(empleado, 40, false);
+        assertEquals(13200, salario);
+    }
+
+    @Test
+    void lanzarExcepcionPorHorasNegativas() {
+        Empleado empleado = new Empleado("Natasha", TipoEmpleado.FULL_TIME, 200);
+        ServicioNomina servicio = new ServicioNomina();
+        assertThrows(IllegalArgumentException.class, () -> servicio.calcularNomina(empleado, -5, false));
+    }
+
+    @Test
+    void lanzarExcepcionSiEmpleadoEsNulo() {
+        ServicioNomina servicio = new ServicioNomina();
+        assertThrows(IllegalArgumentException.class, () -> servicio.calcularNomina(null, 40, false));
+    }
+
+    @Test
+    void lanzarExcepcionSiSalarioSemanalExcedeLimiteSinAutorizacion() {
+        Empleado empleado = new Empleado("Natasha", TipoEmpleado.FULL_TIME, 500);
+        ServicioNomina servicio = new ServicioNomina();
+        assertThrows(IllegalArgumentException.class, () -> servicio.calcularNomina(empleado, 45, false));
     }
 
 }
